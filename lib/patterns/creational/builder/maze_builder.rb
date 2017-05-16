b84a2268f94944d49e4266dca34e30ab1162daad
Vitali Stukov <vstukov@beltechsoftware.com>
2017-05-16 12:30:12 +0300

    Builder

--- lib/patterns/creational/builder/maze_builder.rb
+++ lib/patterns/creational/builder/maze_builder.rb
@@ -2,31 +2,23 @@ module Patterns
   module Creational
     class MazeBuilder
 
-        def initialize(params={})
-          post_initialize(params)
-        end
-
-        def post_initialize(params={})
-          nil
-        end
-
-        def build_maze
-          nil
-        end
-
-        def build_room(nomer)
-          nil
-        end
-
-        def build_door(room_from, room_to)
-          nil
-        end
-
-        def get_maze
-          nil
-        end
-
+      def build_maze
+        nil
       end
+
+      def build_room(nomer)
+        nil
+      end
+
+      def build_door(room_from, room_to)
+        nil
+      end
+
+      def get_maze
+        nil
+      end
+
+    end
 
   end
 end
