e520ea79347eacd84feb9227fc02ea36a25df1e6
Vitali Stukov <vstukov@beltechsoftware.com>
2017-06-14 10:02:28 +0300

    skip iterator

---
+++ lib/patterns/behaviors/iterator/iterator.rb
@@ -0,0 +1,27 @@
+module Patterns
+  module Behaviors
+    module Iterator
+      class Iterator
+
+        def first
+
+        end
+
+        def next
+
+        end
+
+        def is_done
+
+        end
+
+        def current_item
+
+        end
+
+      end
+    end
+  end
+end
+
+
---
+++ spec/lib/patterns/behaviors/iterator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'Iterator' do
+
+  example 'test' do
+
+    skip('in future')
+  end
+end
